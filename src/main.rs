b31f4a8ec194e6786b53197c2c0406386e5bb743
AtLeastEddie8 <ee3de33@icloud.com>
2023-11-08 19:10:14 -0500

    added return value example

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,42 @@ fn main() {
     println!("Hello, world!");
 
     another_function();
+    another_function2(5);
+    print_labeled_measurement(5, 'h');
+    //cant do bc statements dont return values
+    //let x = (let y = 6);
+    {
+        let y = {
+            let x = 3;
+            x + 1
+            //also works, last x is like a return 
+            // let mut x = 3;
+            // x = x + 1;
+            // x
+        };
+
+    println!("The value of y is: {y}");
+    }
+    //Functions with Return Values
+    {
+        let x = five();
+
+        println!("The value of x is: {x}");
+    }
 }
 
 fn another_function() {
     println!("Another function.");
+}
+
+fn another_function2(x: i32) {
+    println!("The value of x is: {x}");
+}
+
+fn print_labeled_measurement(value: i32, unit_label: char) {
+    println!("The measurement is: {value}{unit_label}");
+}
+
+fn five() -> i32 {
+    5
 }
